ccc6a55e8d65da906d42fa38af18b92b0fba402a
teptimgen <teptimgen@yandex.ru>
2015-05-14 15:05:55 +0300

    Update and rename headers.h to Headers.h

---
+++ Headers.h
@@ -0,0 +1,18 @@
+// Copyright 2015 <Timur Teplukhin>
+#pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
+#define FALSE 0
+#define TRUE 1
+typedef unsigned __int8 byte;
+
+#include <iostream>
+#include <cstdio>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::ostream;
+using std::exception;
--- headers.h
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-#include <cstdio>
-#include <iostream> 
-#include <list>
-#include <cstring>
-
-using namespace std;
