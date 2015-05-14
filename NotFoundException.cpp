c410aaff849c7177d34a3742e451c6cf9a724c73
teptimgen <teptimgen@yandex.ru>
2015-05-14 15:12:11 +0300

    Create NotFoundException.cpp

---
+++ NotFoundException.cpp
@@ -0,0 +1,18 @@
+// Copyright 2015 <Timur Teplukhin>
+
+#include "NotFoundException.h"
+#include <string>
+
+using std::to_string;
+
+NotFoundException::NotFoundException(const string& title) : exception() {
+    errMsg = "Cannot find the book (title '" + title + "')";
+}
+
+NotFoundException::NotFoundException(int id) : exception() {
+    errMsg = "Cannot find the book (id " + to_string(id) + ")";
+}
+
+const char* NotFoundException::what() const {
+    return errMsg.c_str();
+}
