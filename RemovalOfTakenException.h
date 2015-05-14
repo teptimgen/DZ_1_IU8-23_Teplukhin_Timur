9c4d36aa79ae30fae0d6028f93135339a8abb9d0
teptimgen <teptimgen@yandex.ru>
2015-05-14 15:13:31 +0300

    Create RemovalOfTakenException.h

---
+++ RemovalOfTakenException.h
@@ -0,0 +1,14 @@
+// Copyright 2015 <Timur Teplukhin>
+#pragma once
+
+#include "Headers.h"
+#include <string>
+
+class RemovalOfTakenException : public exception {
+    string errMsg;
+ public:
+    enum Book;
+    explicit RemovalOfTakenException(const string&);
+    explicit RemovalOfTakenException(int id);
+    const char* what() const throw();
+};
